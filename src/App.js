f81c63005c72cbc497fc249f7f89b16aa9b7aea6
LGE\gyubin1.mun <rbqls6651@naver.com>
2022-06-27 17:22:14 +0900

    초기 커밋

--- src/App.js
+++ src/App.js
@@ -1,23 +1,161 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+        <div className="Header">Header</div>
+        <div className="Banner_Area">
+            <div className="banner_img">
+                <p className="p1">공기청정팬과<br/>
+                    함께 하는<br/>
+                    다채로운 라이프
+                </p>
+                <p>미리 살펴볼까요?</p>
+            </div>
+        </div>
+
+          <div className="Swiper-Area">
+              <div id="card">
+                  <div className="tip-title">
+                      <div className="icon1"></div>
+                          <p className="title1">꼼꼼 리뷰</p>
+                          <div className="all-img" src="./image/icon_next.png"/>
+                  </div>
+
+                  <div className="swiper mySwiper">
+                      <div className="swiper-wrapper">
+                          <div className="swiper-slide">
+                              <div id="use">
+                                  <div className="use-box">
+                                      <a href="https://blog.naver.com/dngusghk/222644239629">
+                                          <img src="./image/img_thum_blog1.png"/>
+                                      </a>
+                                      <a href="https://blog.naver.com/dngusghk/222644239629">
+                                          <div className="use-txt">
+                                              <h2>인테리어 가전 LG퓨리케어 <br/>에어로타워 오브제컬렉션 청정한 공간으로 만들어주는 공기청정팬</h2>
+                                              <p>똑소리나는우새댁</p>
+                                          </div>
+                                      </a>
+                                  </div>
+
+                                  <div className="use-box">
+                                      <a href="https://blog.naver.com/wyh5757/222648106790">
+                                          <img src="./image/img_thum_blog2.png"/>
+                                      </a>
+                                      <a href="https://blog.naver.com/wyh5757/222648106790">
+                                          <div className="use-txt">
+                                              <h2>공기청정팬 LG 퓨리케어 <br/>에어로타워로 실내 인테리어는 물론 깨끗한 공기까지 관리해요</h2>
+                                              <p>핑크비쥬</p>
+                                          </div>
+                                      </a>
+
+                                  </div>
+
+                                  <div className="use-box">
+                                      <a href="https://blog.naver.com/king0655/222641966899">
+                                          <img src="./image/img_thum_blog3.png"/>
+                                      </a>
+
+                                      <a href="https://blog.naver.com/king0655/222641966899">
+                                          <div className="use-txt">
+                                              <h2>LG 퓨리케어 에어로타워<br/> 오브제컬렉션 사계절 청정라이프의 시작</h2>
+                                              <p>하율</p>
+                                          </div>
+                                      </a>
+                                  </div>
+                              </div>
+                          </div>
+                          <div className="swiper-slide">
+                              <div id="use">
+                                  <div className="use-box">
+                                      <a href="https://blog.naver.com/marianne40/222642280548">
+                                          <img src="./image/img_thum_blog4.png"/>
+                                      </a>
+                                      <a href="https://blog.naver.com/marianne40/222642280548">
+                                          <div className="use-txt">
+                                              <h2>공기청정/온풍/선풍 기능이 <br/>합쳐진 LG 퓨리케어 에어로타워 오브제컬렉션 공기청정팬!</h2>
+                                              <p>마리안느</p>
+                                          </div>
+                                      </a>
+                                  </div>
+
+                                  <div className="use-box">
+                                      <a href="https://blog.naver.com/yellowin23/222648146074">
+                                          <img src="./image/img_thum_blog5.png"/>
+                                      </a>
+                                      <a href="https://blog.naver.com/yellowin23/222648146074">
+                                          <div className="use-txt">
+                                              <h2>공간 인테리어가전<br/> LG 퓨리케어 에어로타워 오브제컬렉션으로 홈스타일링 완성</h2>
+                                              <p>꼬꼬마</p>
+                                          </div>
+                                      </a>
+
+                                  </div>
+
+                                  <div className="use-box">
+                                      <a href="https://blog.naver.com/wyh5757/222641992786">
+                                          <img src="./image/img_thum_blog6.png"/>
+                                      </a>
+
+                                      <a href="https://blog.naver.com/wyh5757/222641992786">
+                                          <div className="use-txt">
+                                              <h2>공기청정팬, LG 퓨리케어 <br/>에어로타워 오브제컬렉션 공간 인테리어가전 첫 만남</h2>
+                                              <p>핑크비쥬</p>
+                                          </div>
+                                      </a>
+
+                                  </div>
+                              </div>
+                          </div>
+                          <div className="swiper-slide">
+                              <div id="use">
+                                  <div className="use-box">
+                                      <a href="https://blog.naver.com/miffy2659/222648086376">
+                                          <img src="./image/img_thum_blog7.png"/>
+                                      </a>
+                                      <a href="https://blog.naver.com/miffy2659/222648086376">
+                                          <div className="use-txt">
+                                              <h2>공간 인테리어 가전 <br/>LG 퓨리케어 에어로타워 오브제 컬렉션</h2>
+                                              <p>블러썸</p>
+                                          </div>
+                                      </a>
+                                  </div>
+
+                                  <div className="use-box">
+                                      <a href="https://blog.naver.com/jayonce1004/222655712618">
+                                          <img src="./image/img_thum_blog8.png"/>
+                                      </a>
+                                      <a href="https://blog.naver.com/jayonce1004/222655712618">
+                                          <div className="use-txt">
+                                              <h2>원하는 온도와 부드러운 바람이<br/> 좋은 에어로타워 공기청정팬 :) </h2>
+                                              <p>소루앤제이</p>
+                                          </div>
+                                      </a>
+
+                                  </div>
+
+                                  <div className="use-box">
+                                      <a href="https://blog.naver.com/babysonn1/222644730291">
+                                          <img src="./image/img_thum_blog9.png"/>
+                                      </a>
+
+                                      <a href="https://blog.naver.com/babysonn1/222644730291">
+                                          <div className="use-txt">
+                                              <h2>LG 퓨리케어 에어로타워 <br/>오브제컬렉션 공기청정팬으로 홈인테리어를 살리다</h2>
+                                              <p>그레이</p>
+                                          </div>
+                                      </a>
+
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                      <div className="swiper-pagination"></div>
+                  </div>
+              </div>
+          </div>
+
+
     </div>
   );
 }
